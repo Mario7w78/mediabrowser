559582350561adcdf1701ac5f92f39cd9d4711b4
Mario7w78 <mariox0603@gmail.com>
2025-04-13 23:46:40 -0500

    some changes..

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ function App() {
 
   return (
     <main className="bg-gray-800 w-screen h-screen grid grid-rows-5 grid-cols-15 text-white">
-
-      <AddButton />
+      
+      <AddButton /> 
 
       <div className=" row-span-2 row-start-4 border-r-1 border-gray-600 grid grid-rows-3 justify-items-center">
         <button className="row-start-2 w-10 h-10 my-10 cursor-pointer "><img itemType="svg" src="/user.svg" /></button>
       </div>
+      
       <div className="col-span-14 border-b-1 border-gray-600">2</div>
       <div className="row-span-4 col-span-9">3</div>
     </main> 
--- src/components/addButton/Addbutton.jsx
+++ src/components/addButton/Addbutton.jsx
@@ -8,16 +8,16 @@ export default function AddButton() {
 
     return ( 
             <>
-            <div className="row-span-3 border-r-1 border-gray-600 grid grid-row-10 relative">
-                <button onClick={handleClick} className="w-15 h-15 my-10 cursor-pointer text-5xl flex justify-center">+</button>
+            <div className="row-span-3 border-r-1 border-gray-600 grid grid-row-10 relative justify-center">
+                <button onClick={handleClick} className="w-15 h-15 my-10 cursor-pointer text-5xl flex justify-center items-baseline ">+</button>
                 {createDir && 
-                <div>
-                    <form className="bg-gray-700 absolute top-24 left-100 w-70 h-70 rounded-xl grid grid-rows-10 grid-cols-4 gap-x-2">
+                <div >
+                    <form className=" absolute bg-gray-700 top-30 left-120 w-70 h-70 rounded-xl grid grid-rows-10 grid-cols-4 gap-x-2 z-10">
                         <p className='col-span-full text-center pt-1 font-bold font-mono'>Create new directory</p>
                         
 
-                        <button type='submit' className='font-bold font-mono bg-gray-500 text-gray-50 p-1 rounded-xl -row-start-3 col-start-2 flex items-center justify-center'  onClick={handleClick}>Create</button>
-                        <button className='font-bold font-mono bg-gray-500 text-gray-50 p-1 rounded-xl -row-start-3 col-start-3 flex items-center justify-center'  onClick={handleClick}>Cancel</button>
+                        <button type='submit' className='cursor-pointer font-bold font-mono bg-gray-500 text-gray-50 p-1 rounded-xl -row-start-3 col-start-2 flex items-center justify-center'  onClick={handleClick}>Create</button>
+                        <button className='cursor-pointer font-bold font-mono bg-gray-500 text-gray-50 p-1 rounded-xl -row-start-3 col-start-3 flex items-center justify-center'  onClick={handleClick}>Cancel</button>
                     </form>
                 </div>
             }
